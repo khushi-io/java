d880883f107ce4a1b8b7f93c3af726109eef3bbb
Khushi Chile <khushichile2004@gmail.com>
2026-04-13 14:24:05 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
-
-        System.out.print("Choose operation (+, -, *, /): ");
-        char op = sc.next().charAt(0);
+        double num1 = 10;
+        double num2 = 5;
+        char op = '+';
 
         double result;
 
@@ -43,7 +35,5 @@ public class Calculator {
             default:
                 System.out.println("Invalid operator!");
         }
-
-        sc.close();
     }
 }
